c004b52e0c33d045cde8afc1e110dd0b2c232a45
Wolgerrom5 <sofyaviktorovna26@gmail.com>
2024-03-07 12:07:50 +0700

    one

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-category = int(input())
-month_name = [range(12)]
+
+import ru_local as ru
+category = int(input({ru.TYPE}))
+month_name = [ru.NAME_MONTH]
 
 income = []
 for month in range(12):
+    print({ru.INCOME})
     income.append(float(input()))
 gross_income = sum(income)
 
 benefit = []
 for month in range(12):
+    print({ru.BENEFIT})
     benefit.append(float(input()))
 total_benefit = sum(benefit)
 
@@ -19,12 +23,12 @@ tax_rate = [10, 15, 25, 28, 33, 35, 39.6]
 
 def total_tax(bottom_limits):
     amount = 0
-    for j in range(12:
-        for i in range(len(bottom_limits)):
-            if i+1 in range(len(bottom_limits)) and bottom_limits[i] <= tax_base[j] < bottom_limits[i+1]:
-                amount += (tax_base[j] - bottom_limits[i]) * tax_rate[i] * 0.01
+    for j in range(12):
+        for n in range(len(bottom_limits)):
+            if n+1 in range(len(bottom_limits)) and bottom_limits[n] <= tax_base[j] < bottom_limits[n+1]:
+                amount += (tax_base[j] - bottom_limits[n]) * tax_rate[n] * 0.01
             else:
-                amount += (tax_base[j] - bottom_limits[i]) * tax_rate[-1] * 0.01
+                amount += (tax_base[j] - bottom_limits[n]) * tax_rate[-1] * 0.01
 
     return amount
 
